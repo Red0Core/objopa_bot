93dc3889493051377c6f3f4ba49ed2183b8d13fd
Red0Core <red0.0core@gmail.com>
2025-04-17 00:52:11 +0300

    Обновил редис клиент для прееподключения

--- core/redis_client.py
+++ core/redis_client.py
@@ -1,4 +1,11 @@
 from redis.asyncio import Redis
+from redis.backoff import ExponentialBackoff
+from redis.asyncio.retry import Retry
+from redis.exceptions import (
+   BusyLoadingError,
+   ConnectionError,
+   TimeoutError
+)
 from core.config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
 
 redis = Redis(
@@ -6,4 +13,6 @@ redis = Redis(
     port=REDIS_PORT,
     password=REDIS_PASSWORD,
     decode_responses=True,
+    retry=Retry(ExponentialBackoff(), 5),
+    retry_on_error=[BusyLoadingError, ConnectionError, TimeoutError]
 )
